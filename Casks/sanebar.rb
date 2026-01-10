ebb1770166f1756baa3b5d97e5fa23b4c496a4bf
SJ <stephanjoseph2007@gmail.com>
2026-01-09 19:59:43 -0500

    Bump to v1.0.3 - WindowServer crash fix

--- Casks/sanebar.rb
+++ Casks/sanebar.rb
@@ -1,6 +1,6 @@
 cask "sanebar" do
-  version "1.0.2"
-  sha256 "2725d6c329637c8252b15b6bcef21f892c88ccb7c338a16ea9386d6073ed1f59"
+  version "1.0.3"
+  sha256 "bdf1bdc6b2df67c3699f76b77d031d2567b48c2a216b16f9f65f1f8a0db2f96d"
 
   url "https://github.com/stephanjoseph/SaneBar/releases/download/v#{version}/SaneBar-#{version}.dmg"
   name "SaneBar"
